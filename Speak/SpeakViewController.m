6c1a794ba3c60b94196e7202f61de4b5bea321a8
Ian Perry <ianperry90@gmail.com>
2014-02-15 07:54:28 -0500

    Initial Commit

---
+++ Speak/SpeakViewController.m
@@ -0,0 +1,29 @@
+//
+//  SpeakViewController.m
+//  Speak
+//
+//  Created by Ian Perry on 2/15/14.
+//  Copyright (c) 2014 SpeakTeam. All rights reserved.
+//
+
+#import "SpeakViewController.h"
+
+@interface SpeakViewController ()
+
+@end
+
+@implementation SpeakViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
